d352d35d118dc2421716cc2212566d0787a9b1cd
Nifyy <nifsam@outlook.com>
2025-05-17 19:50:49 +0000

    Yes

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
 
@@ -15,7 +14,7 @@ class _HomescreenState extends State<Homescreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("Assets/background.png"),
+            image: AssetImage("assets/background.png"),
             fit: BoxFit.fill,
          ),
        ),
@@ -24,3 +23,31 @@ class _HomescreenState extends State<Homescreen> {
   }
 }
 
+class TopBar extends StatefulWidget implements PreferredSizeWidget{
+  const TopBar({super.key});
+
+  @override
+  State<TopBar> createState() => _TopBarState();
+
+  @override
+ 
+  Size get preferredSize => Size(double.maxFinite, 80);
+}
+
+class _TopBarState extends State<TopBar> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      child: Material(
+        color: Color.fromARGB(255, 26, 26, 26),
+        child: Container(
+          decoration: BoxDecoration(color: Color.fromARGB(255, 26, 26, 26)),
+          child: Stack(
+            children: [ClipRRect]
+          )
+        ),
+      ),
+    );
+  }
+}
